7829224fb5dd0f2894957d36f267f019eb17eb23
YCHULHEE <ych9411@naver.com>
2020-08-11 15:11:12 +0900

    v1

--- Main.cs
+++ Main.cs
@@ -15,6 +15,32 @@ namespace Travel
         public Main()
         {
             InitializeComponent();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void mainUC1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
